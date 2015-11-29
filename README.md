3adb9d87e9d902de30c8ac130d3d0edd30b9cfa3
Yurii Kovalenko <ua.preston@gmail.com>
2015-11-29 21:45:59 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # angular-pull-down-to-refresh
 Pull down to refresh for mobile
+
+## Installation
++ Install with [bower](http://bower.io/)
+
+``` bash
+$ bower install angular-pull-down-to-refresh --save
+```
++ Include the required files:
+
+``` html
+<link rel="stylesheet" href="bower_components/angular-pull-down-to-refresh/angular-pull-down-to-refresh.css">
+<script src="bower_components/angular-pull-down-to-refresh/angular-pull-down-to-refresh.js"></script>
+```
++ Inject the `c12o16h1.pullDownToRefresh` module into your app:
+
+``` javascript
+angular.module('myApp', ['c12o16h1.pullDownToRefresh']);
+```
+## Usage
+
+``` html
+<div class="container-fluid">
+    <div class="container-panel" pull-down-to-refresh="pathReload()">
+    </div>
+</div>
+```
+
+``` javascript
+'use strict';
+myApp
+    .controller('Test', ['$scope','$timeout',  function($scope, $timeout) {
+        $scope.pathReload = function(){
+            $timeout(function(){
+                $window.location.reload();
+            },50);
+        };
+    }]);
+```
+
+## Authors
+
+**Yurii Kovalenko**
+
++ http://github.com/c12o16h1
+
+## Copyright and license
+
+  The MIT License
+
+  Copyright (c) 2015 Yurii Kovalenko
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ angular-pull-down-to-refresh.js
@@ -0,0 +1,90 @@
+/**
+ * Created by Yurii Kovalenko c12o16h1@gmail.com on 11/29/15.
+ */
+(function (window, document, undefined) {
+    'use strict';
+    angular.module('c12o16h1.pullDownToRefresh', []).constant('pullDownToRefreshConfig', {
+        treshold: 60,
+        debounce: 400,
+        text: {
+            pull: 'pull to refresh',
+            ready: 'release to refresh',
+            loading: 'refreshing...',
+            hide: 'refreshed'
+        },
+
+        icon: {
+            pull: 'glyphicon glyphicon-arrow-down',
+            ready: 'glyphicon glyphicon-play',
+            loading: 'glyphicon glyphicon-refresh',
+            hide: 'glyphicon glyphicon-arrow-up'
+        }
+    }).directive('pullDownToRefresh', [
+        '$compile',
+        '$timeout',
+        '$q',
+        'pullDownToRefreshConfig',
+        function ($compile, $timeout, $q, pullDownToRefreshConfig) {
+            return {
+                scope: true,
+                restrict: 'A',
+                transclude: true,
+                templateUrl: 'angular-pull-down-to-refresh.tpl.html',
+                compile: function compile(tElement, tAttrs, transclude) {
+                    return function postLink(scope, iElement, iAttrs) {
+                        var config = angular.extend({}, pullDownToRefreshConfig, iAttrs);
+                        var targetEl = window.ptr = iElement.children()[0];
+                        var startPosY;
+                        scope.text = config.text;
+                        scope.icon = config.icon;
+                        scope.status = 'pull';
+                        var shouldReload = false;
+                        var setStatus = function (status) {
+                            shouldReload = status === 'ready';
+                            scope.$apply(function () {
+                                scope.status = status;
+                            });
+                        };
+                        iElement.bind('touchstart', function (e) {
+                            startPosY = e.originalEvent.touches[0].clientY;
+                        });
+                        iElement.bind('touchmove', function (e) {
+                            targetEl.style.webkitTransitionDuration = 0;
+                            targetEl.style.margin = '0 auto';
+                            var curPosY = e.originalEvent.touches[0].clientY - startPosY;
+                            if (curPosY > config.treshold && !shouldReload) {
+                                setStatus('ready');
+                            } else if (curPosY < config.treshold && shouldReload) {
+                                setStatus('pull');
+                            }
+                        });
+                        iElement.bind('touchend', function (e) {
+                            if (!shouldReload)
+                                return;
+                            setStatus('loading');
+                            var start = +new Date();
+                            $q.when(scope.$eval(iAttrs.pullDownToRefresh)).then(function () {
+                                var elapsed = +new Date() - start;
+                                $timeout(function () {
+                                    targetEl.style.margin = '';
+                                    targetEl.style.webkitTransitionDuration = '';
+                                    scope.status = 'hide';
+                                }, elapsed < config.debounce ? config.debounce - elapsed : 0);
+                            });
+                        });
+                        scope.$on('$destroy', function () {
+                            iElement.unbind('touchmove');
+                            iElement.unbind('touchend');
+                        });
+                    };
+                }
+            };
+        }
+    ]);
+    angular.module('c12o16h1.pullDownToRefresh').run([
+        '$templateCache',
+        function ($templateCache) {
+            $templateCache.put('angular-pull-down-to-refresh.tpl.html', '<div class=\'pull-down-to-refresh\'>\n' + '  <i ng-class=\'icon[status]\'></i>&nbsp;\n' + '  <span ng-bind=\'text[status]\'></span>\n' + '</div>\n' + '<div ng-transclude></div>\n');
+        }
+    ]);
+}(window, document));
